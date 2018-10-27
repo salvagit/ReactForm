941679a689bee9cf667b60191f51c482b62ca4dd
salva <s@salvadorp.com>
2018-10-27 18:26:21 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Register from "./containers/Register";
+
+class App extends Component {
+  render() {
+    return (
+      <Register />
+    );
+  }
+}
+
+export default App;
